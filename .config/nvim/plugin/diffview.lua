7acee6d3e6550263eceb0128a9593d9fe7cabbac
gam-perarc <gam.arcanys.personal@gmail.com>
2022-11-07 13:26:36 +0800

    diffview: add mapping for current file history

--- .config/nvim/plugin/diffview.lua
+++ .config/nvim/plugin/diffview.lua
@@ -5,3 +5,6 @@ vim.api.nvim_set_keymap('n', '<leader>dv', [[<CMD> DiffviewOpen<CR>]],
 
 vim.api.nvim_set_keymap('n', '<leader>dh', [[<CMD> DiffviewFileHistory<CR>]],
                         {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<leader>df', [[<CMD> DiffviewFileHistory %<CR>]],
+                        {noremap = true})
